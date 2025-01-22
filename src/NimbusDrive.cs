9a6881557d2b1885b8b28e9bf6ccaa4ef46ffa14
github-is-garbage <177191469+github-is-garbage@users.noreply.github.com>
2025-01-22 12:13:47 -0500

    Humble beginnings

---
+++ src/NimbusDrive.cs
@@ -0,0 +1,10 @@
+﻿namespace NimbusDrive
+{
+    public class NimbusDrive
+	{
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
